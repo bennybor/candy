6ab8d969fe315814b14991415bb7eaaa5018f310
unknown <l101@LABLIB01.ruppindom>
2013-04-25 13:16:17 +0300

    Omri's Toffy Commit

---
+++ Candy1/Toffi.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Candy1
+{
+    public partial class Toffi : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
